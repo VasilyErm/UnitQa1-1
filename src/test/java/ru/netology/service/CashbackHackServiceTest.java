0efd0a07789680c82c91f697a66243512ebc9bb9
Vasily Ermolaev <vasily.erm@gmail.com>
2023-07-19 23:33:38 +0300

    Initional comit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,31 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void testGetCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+
+    @org.junit.Test
+    public void testNeedToGetCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
 
 }
